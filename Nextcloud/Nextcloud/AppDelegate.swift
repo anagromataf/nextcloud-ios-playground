e6e2f2d3c2aa63a9c63c628d2ae59b6e21977c07
Tobias Kräntzer <info@tobias-kraentzer.de>
2017-01-23 22:02:58 +0100

    Initial Commit

---
+++ Nextcloud/Nextcloud/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Nextcloud
+//
+//  Created by Tobias Kraentzer on 23.01.17.
+//  Copyright © 2017 Nextcloud. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+    
+}
