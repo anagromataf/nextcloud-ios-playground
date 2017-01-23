55cf5118e151c73c675810a09b33eb5bebf0ae97
Tobias Kräntzer <info@tobias-kraentzer.de>
2017-01-23 22:10:26 +0100

    Add targets for UI and Core

---
+++ Nextcloud/NextcloudCore/NextcloudCore.h
@@ -0,0 +1,19 @@
+//
+//  NextcloudCore.h
+//  NextcloudCore
+//
+//  Created by Tobias Kraentzer on 23.01.17.
+//  Copyright © 2017 Nextcloud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NextcloudCore.
+FOUNDATION_EXPORT double NextcloudCoreVersionNumber;
+
+//! Project version string for NextcloudCore.
+FOUNDATION_EXPORT const unsigned char NextcloudCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NextcloudCore/PublicHeader.h>
+
+
